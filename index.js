38c8ab03018d73b411b253660493aac4ca0db1cd
Jacob Klimenko <droidedjake@gmail.com>
2021-08-08 21:04:34 -0400

    commit tree 2babd29e00da560dccec33f3cd46d253c356c265

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
-}
+    static capitalize(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    };
+
+    static sanitize(string) {
+        return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+    };
+
+    static titleize(string) {
+        const inapplicable = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+        
+        let words = string.split(' ');
+        words = words.map(function(word) {
+            let invalid = inapplicable.find(e => e === word);
+            if (!invalid || (words.indexOf(word) === 0)) {
+                return this.capitalize(word);
+            } else {
+                return word;
+            };
+        }, this);
+        
+        return words.join(' ');
+    };
+};
